5218d43ffef81b5c709284a4c6c5ae5eff87f2f8
Kutman <kubanchbekovkutman9@gmail.com>
2024-02-28 17:27:21 +0600

    hw-7

---
+++ hw-7/script.js
@@ -0,0 +1,89 @@
+// const priceBool = {
+//   name: "bool",
+//   price: 2095,
+// };
+// console.log(priceBool);
+
+// priceBool.price = priceBool.price + 500;
+
+// priceBool.delivery_time = "3кун";
+
+// const comparePrice = (product1, product2) => {
+//   const objektSena = {
+//     qwer: "боскедболный мач",
+//     product1: 2095,
+//     product2: 1090,
+//   };
+//   if (Math.max((product1 = product2))) {
+//     return (qwer = product1), "дорогой товар";
+//   } else if (Math.max(product1 === product2)) {
+//     return qwer === product2,"менше чем того";
+//   }
+// };
+// console.log(comparePrice(2095, 1090));
+
+// const product1 = {
+//   name: "basketball",
+//   price: 2095,
+// };
+// const product2 = {
+//   name: "socks",
+//   price: 1090,
+// };
+// const myObjektSrovneniy = (product1, product2) => {
+//   if (product1.price === product2.price && product2.name === product1.name) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+// console.log(myObjektSrovneniy(product1, product2));
+
+// let good = "Good Morning";
+// let text = "TESTING";
+// console.log(good.toLowerCase("Good Morning"));
+// console.log(text.toLowerCase("TESTING"));
+
+// let sap = "GOOD MORNING";
+// let sap2 = "TESTING";
+
+// console.log(sap.repeat(2));
+// console.log(sap2.repeat(4));
+
+// let button = document.getElementById("Js");
+// console.log(button);
+
+// let money = document.getElementById('Orel')
+// let div = document.getElementById('div')
+
+
+// function playOriol(qwery) {
+//     div.innerText = "Ваш выбор " + qwery;
+// }
+// money.addEventListener('click',()=>{
+//     playOriol ('Орёл')
+// })
+
+// let projekt = document.getElementById('Reshka')
+// let progect  =document.getElementById('fhjkf')
+
+// function playOriol(otvet) {
+//     progect.innerText = "Ваш выбор " + otvet;
+// }
+// projekt.addEventListener('click',()=>{
+//     playOriol ('Решка')
+// })
+
+
+
+
+// localStorage.setItem('Number','0707507530'
+// )
+// let kutiProjekt = localStorage.getItem('Number')
+// console.log(kutiProjekt);
+
+
+// localStorage.setItem('simon','Kutman')
+// let kutiProjekt2 =localStorage.getItem('simon')
+// console.log(kutiProjekt2);
+
